6b96f52788ce4436791f329ab165f3972ae6072b
maurice diong <mdiong00@gmail.com>
2024-01-17 06:53:57 -0500

    fun loop lab

--- lib/looping.py
+++ lib/looping.py
@@ -3,11 +3,27 @@
 def happy_new_year():
     # code goes here!
     pass
+    i=10
+    while i > 0 :
+        print(f"{i}")
+        i -=1
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
     # code goes here!
     pass
+    return [integer*integer for integer in int_list]
 
 def fizzbuzz():
     # code goes here!
     pass
+    for i in range(100) :
+        if (i+1) % 3 == 0 and (i+1) % 5 == 0 :
+            print ("FizzBuzz")
+        elif (i+1) % 5 == 0 :
+            print("Buzz")
+        elif (i+1) % 3 == 0 :
+            print("Fizz")
+        else :
+            print(f"{i+1}")
